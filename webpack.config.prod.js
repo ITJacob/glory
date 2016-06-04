3d19b860cf90255ba3b15a68ec3de18e06b946c1
Dong Wang <jacob.wd8816@gmail.com>
2016-06-04 18:36:13 +0800

    roll back webpack

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -30,9 +30,15 @@ module.exports = {
   },
 
   plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production'),
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
       }
     }),
     new ExtractTextPlugin("app.css"),
